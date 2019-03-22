720777849785016467ea8895c42b92c5ada43c5b
jfzanato <jfzanato@hotmail.com>
2019-03-22 16:48:16 -0300

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,13 @@
+  new Vue({
+    el: '#app',
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+      .get('http://sandbox-api.lomadee.com/v2/1548274999869d74cbd8d/product/_search?sourceId=35561801&keyword=motorola')
+        .then(response => (this.info = response.data.products))
+    }
+  })
